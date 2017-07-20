211781996946fc483b5143107a8528de59231db6
王敬博 <470665207@qq.com>
2017-07-20 19:59:01 +0800

    更新测试

--- __tests__/app.js
+++ __tests__/app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
-describe('generator-test:app', () => {
+describe('generator-mobx-react:app', () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
       .withPrompts({someAnswer: true});
